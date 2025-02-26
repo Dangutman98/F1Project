acf71fadda9bbdd4f76da62669a1161d8a629c08
Dan <dangutman.98@gmail.com>
2025-02-26 15:06:00 +0200

    added in appsetting.json the local host server connection name

---
+++ server/DAL/userDB.cs
@@ -0,0 +1,16 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace server.Data
+{
+    public class userDB : DbContext
+    {
+        public userDB(DbContextOptions<userDB> options)
+            : base(options)
+        {
+        }
+
+        // Define a DbSet for your Users table
+        public DbSet<User> Users { get; set; }
+    }
+}
